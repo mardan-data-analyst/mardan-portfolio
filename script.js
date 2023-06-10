5a6148cbef37cf3b1a105faa155bd8a8edc188d9
Mardan Mirzaguliyev <103627214+mardan-data-analyst@users.noreply.github.com>
2023-06-10 11:49:51 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ const projectsData = [
   {
     title: 'Analysis New York City Bike data',
     description: 'This analysis was a part 5 day long data analytics tutorial provided by Career Foundry.',
-    imageUrl: 'main/images/city-bike.jpg',
+    imageUrl: 'images/city-bike.jpg',
     demoUrl: 'https://docs.google.com/spreadsheets/d/1t1Lh2ZEDC30OYTowz0_FOHp2fDMt41UoAEPB2jfE67I/edit?usp=sharing'
   },
   {
-    title: 'Project 2',
-    description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    imageUrl: 'path/to/project2-image.jpg',
-    demoUrl: 'path/to/project2-demo.html'
+    title: 'KPMG virtual Internship',
+    description: 'Virtual internship on The Forage web site which involved tasks from KPMG. Tasks and tools applied to solve them: Data Quality Assessment using Excel and R
+    Data Insights using R Data Insights and Presentation using Tableau',
+    imageUrl: 'images/kpmg.jpg',
+    demoUrl: ''
   }
 ];
 
