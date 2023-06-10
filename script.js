0d4f5d3aa914bde6404530bd82b1d733a61bdb58
Mardan Mirzaguliyev <103627214+mardan-data-analyst@users.noreply.github.com>
2023-06-10 11:33:38 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const projectsData = [
   {
     title: 'Analysis New York City Bike data',
     description: 'This analysis was a part 5 day long data analytics tutorial provided by Career Foundry.',
-    imageUrl: 'https://github.com/mardan-data-analyst/mardan-data-analyst.github.io/blob/main/images/city-bike.jpg',
+    imageUrl: 'images/city-bike.jpg',
     demoUrl: 'https://docs.google.com/spreadsheets/d/1t1Lh2ZEDC30OYTowz0_FOHp2fDMt41UoAEPB2jfE67I/edit?usp=sharing'
   },
   {
